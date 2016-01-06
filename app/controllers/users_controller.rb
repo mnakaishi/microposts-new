619b16533cb40a54823cb7ca0412b78b315e7da3
mnakaishi <nakaishi@mazak.it>
2016-01-06 09:53:34 +0000

    user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,28 @@ class UsersController < ApplicationController
     end
   end
   
+  def update # 追加
+    if @user.update(user_params)
+      flash[:success] = "profile updated"
+      redirect_to user_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def edit # 追加
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to root_url, alert: 'Non authorization'
+    end
+  end
+  
+  
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :region, :profile)
   end
 end
 
