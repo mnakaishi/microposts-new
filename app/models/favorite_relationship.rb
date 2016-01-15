d93952c70b6e7a978307afc59f54b38a794ce6a4
mnakaishi <nakaishi@mazak.it>
2016-01-15 05:19:25 +0000

    favorite trial

--- app/models/favorite_relationship.rb
+++
@@ -1,6 +0,0 @@
-class FavoriteRelationship < ActiveRecord::Base
-
-  belongs_to :user, class_name: "User"
-  belongs_to :micropost, class_name: "Micropost"
-  
-end
--- db/migrate/20160112054549_create_favorite_relationships.rb
+++
@@ -1,8 +0,0 @@
-class CreateFavoriteRelationships < ActiveRecord::Migration
-  def change
-    create_table :favorite_relationships do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
